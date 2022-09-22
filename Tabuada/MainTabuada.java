6b664f916490df5945ff906a9f3f7bf7c05b71af
Amanda Fantuzze <105981624+FriendlyCheesecake@users.noreply.github.com>
2022-09-22 22:06:10 +0900

    Add files via upload

---
+++ Tabuada/MainTabuada.java
@@ -0,0 +1,19 @@
+package Tabuada;
+
+public class MainTabuada {
+    public static void main(String[] args) {
+        final int multiplicador = 7;
+        System.out.printf("Olá, veja a seguir a tabuada do %s: ", multiplicador);
+        System.out.printf("\n" + multiplicador + " X 1 = %s ", multiplicador * 1);
+        System.out.printf("\n" + multiplicador + " X 2 = %s ", multiplicador * 2);
+        System.out.printf("\n" + multiplicador + " X 3 = %s ", multiplicador * 3);
+        System.out.printf("\n" + multiplicador + " X 4 = %s ", multiplicador * 4);
+        System.out.printf("\n" + multiplicador + " X 5 = %s ", multiplicador * 5);
+        System.out.printf("\n" + multiplicador + " X 6 = %s ", multiplicador * 6);
+        System.out.printf("\n" + multiplicador + " X 7 = %s ", multiplicador * 7);
+        System.out.printf("\n" + multiplicador + " X 8 = %s ", multiplicador * 8);
+        System.out.printf("\n" + multiplicador + " X 9 = %s ", multiplicador * 9);
+        System.out.printf("\n" + multiplicador + " X 10 = %s ", multiplicador * 10);
+
+    }
+}
